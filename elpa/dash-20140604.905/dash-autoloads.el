f07868881cfb961118f0802a4c4c7508a4bd8497
Adam Alix <adam.alix@gmail.com>
2014-08-16 18:22:54 +0000

    update packages, adds go modes

--- elpa/dash-20140604.905/dash-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21391 54023
-;;;;;;  236979 0))
-
-;;;***
-
-(provide 'dash-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dash-autoloads.el ends here
